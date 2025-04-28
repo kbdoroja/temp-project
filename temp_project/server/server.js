c1ee3a4fee9eb1786eaacfac9ce3ba8ec87e4480
Yanika Tauro <yjtauro@up.edu.ph>
2025-04-28 20:14:02 +0800

    build: established connection to MongoDB database

--- temp_project/server/server.js
+++ temp_project/server/server.js
@@ -1,7 +1,7 @@
 // Server.js connects MongoDB database using Mongoose
 
 import express from 'express';
-// import router from './router.js';
+import router from './router.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// router(app);
+router(app);
 
 
 app.listen(3000, (err) => {
