ac07fb52bb6ae579f14d8d579e4420d7bafb3912
Yanika Tauro <yjtauro@up.edu.ph>
2025-04-28 20:09:33 +0800

    refactor: changed MongoDB connection error handling

--- temp_project/server/controller.js
+++ temp_project/server/controller.js
@@ -6,14 +6,9 @@ import mongoose from 'mongoose';
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log('Connected to MongoDB Atlas');
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-  })
-  .catch((error) => {
-    console.error('Connection error', error.message);
-  });
+})
+  .then(() => console.log('Connected to MongoDB Atlas'))
+  .catch((err) => console.error('Connection error', err.message));
 
 // INSERT HERE SCHEMA DESIGN
 
